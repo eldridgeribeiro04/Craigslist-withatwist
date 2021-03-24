baa8fd7e0a2ccf8fe8f29a9c64fb34e4a36b9b0c
Eldridge ribeiro <eldridgeriberio@gmail.com>
2021-03-24 22:12:13 +0000

    hid secret key

--- codedaddieslist/codedaddieslist/settings.py
+++ codedaddieslist/codedaddieslist/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+from Secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xgf97!h2likddxfz1!ri0idq*#0-0llsy#b41kyb0k-h9llvd8'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
